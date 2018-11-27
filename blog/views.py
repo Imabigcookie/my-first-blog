b658d2e0392f597713ee103b65790492956dfe70
Alex <sasher9851@gmail.com>
2018-11-27 21:07:20 +0300

    fails defected

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
-# Create your views here.
-=======
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
->>>>>>> 2ea9cf15246ae8e7ad60c35f930519679045041d
