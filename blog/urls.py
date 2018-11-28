d00d05d80f0c3fb37dfeaa4138360ffd568f9973
Alex <sasher9851@gmail.com>
2018-11-28 21:43:09 +0300

    Templates and css was added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.urls import  re_path, path, include
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name = 'post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name = 'post_detail'),
+    
 ]
